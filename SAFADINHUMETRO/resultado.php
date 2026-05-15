bae545f53e127f93d56007fa04bdf1888c687718
hidamari <20251foz0020011@estudantes.ifpr.edu.br>
2026-05-15 15:40:29 -0300

    Update resultado.php

--- SAFADINHUMETRO/resultado.php
+++ SAFADINHUMETRO/resultado.php
@@ -1,5 +1,8 @@
 <?php
 
+$anjo = 0;
+$safadeza = 0;
+
 $dia = $_REQUEST['campo_dia'];
 $mes = $_REQUEST['campo_mes'];
 $ano = $_REQUEST['campo_ano'];
@@ -30,12 +33,36 @@ function somaDigitos(int $numero){
 
 function safadao(int $dia, int $mes, int $ano){
 
+    global $anjo;
+    global $safadeza;
+
     $safadeza = somatorio($mes) + somaDigitos($ano) * (50 - $dia) / 10;
-    print "Nível de safadeza: $safadeza <br>";
+    //print "Nível de safadeza: $safadeza <br>";
 
     $anjo = 100 - $safadeza;
-    print "Nível de anjeza: $anjo";
+    //print "Nível de anjeza: $anjo";
     
 }
 
 print safadao($dia, $mes, $ano);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WESLEI</title>
+</head>
+<body>
+    
+    <h2><?php print "Nível de safadeza: $safadeza, ai tem o nível de anejza: $anjo"; ?></h2>
+    <a href="index.hmtl">RECALCULAR</a>
+
+    <img src="https://i.ytimg.com/vi/tnQzkODdKQ8/sddefault.jpg">
+    <img src="https://conteudo.imguol.com.br/c/entretenimento/44/2016/10/13/nao-tem-um-acontecimento-que-nao-vire-meme-com-wesley-safadao-na-epoca-da-copa-quando-comecou-a-ganhar-projecao-nacional-o-cearense-virou-figurinha-do-album-da-selecao-so-na-internet-claro-o-1476400940841_142x200.jpg.webp">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYRcevKJpsksRmNw8h-_8mwZ1Ry1vD-O6Vsw&s">
+
+</body>
+</html>
